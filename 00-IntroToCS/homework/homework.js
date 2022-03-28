f4efbd3909350c5c4fd4eddcb58769f504e52e84
Yocsan Levi <yocsanlevi@gmail.com>
2022-03-28 09:29:03 -0600

    primer comentario de tarea

---
+++ 00-IntroToCS/homework/homework.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+ 
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+/* Para convertir de numeros decimales a binarios tengo que hacer lo siguiente: */
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
